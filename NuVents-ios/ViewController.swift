2b4c1ec89ee4f322a7efa208eab9dda863c54aad
Hersh Amin <h.amin@utexas.edu>
2015-04-26 15:28:36 -0500

    Initial Commit

---
+++ NuVents-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NuVents-ios
+//
+//  Created by hersh amin on 4/26/15.
+//  Copyright (c) 2015 NuVents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
