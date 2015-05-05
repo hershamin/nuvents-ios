063e254dd5408ffdefe9a028ad58586db37fb3d5
Hersh Amin <h.amin@utexas.edu>
2015-05-05 14:31:35 -0500

    Added example communication implementation with NuVents Backend

--- NuVents-ios/ViewController.swift
+++ NuVents-ios/ViewController.swift
@@ -8,22 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NuVentsBackendDelegate {
+    
+    var api:NuVentsBackend?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.20, zoom: 6)
+        var camera = GMSCameraPosition.cameraWithLatitude(30.3077609, longitude: -97.7534014, zoom: 9)
         var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
         mapView.myLocationEnabled = true
         self.view = mapView
         
         var marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2DMake(30.2766, -97.734)
+        marker.title = "Austin"
+        marker.snippet = "TX"
         marker.map = mapView
+        
+        api = NuVentsBackend(delegate: self, server: "repo.nuvents.com:1026", device: "test")
         
     }
 
@@ -31,7 +35,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    // MARK: NuVents backend delegate methods
+    func nuventsServerDidConnect() {
+        println("NuVents backend connected")
+        api?.pingServer()
+        var loc = CLLocationCoordinate2D(latitude: 30.2766, longitude: -97.7324)
+        api?.getNearbyEvents(loc, radius: 500)
+    }
+    
+    func nuventsServerDidDisconnect() {
+        println("NuVents backend disconnected")
+    }
+    
+    func nuventsServerDidGetNewData(channel: NSString, data: AnyObject) {
+        //println("NuVents channel \(channel) with data \(data)")
+    }
+    
+    func nuventsServerDidRespondToPing(response: NSString) {
+        //
+    }
+    
+    func nuventsServerDidReceiveError(type: NSString, error: NSString) {
+        //
+    }
+    
+    func nuventsServerDidReceiveStatus(type: NSString, status: NSString) {
+        //
+    }
+    
+    func nuventsServerDidReceiveNearbyEvent(event: JSON) {
+        println("OBJ")
+        for (key: String, subJson: JSON) in event {
+            println("    \(key): \(subJson)")
+        }
+    }
+    
+    func nuventsServerDidReceiveEventDetail(event: JSON) {
+        //
+    }
 
 }
 
