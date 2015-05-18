510cb4d62fc51ee21d664c3e8c846890c1d9acea
Hersh Amin <h.amin@utexas.edu>
2015-05-18 15:03:48 -0500

    DetailView is just a single webview instead of having separate for PartialView

--- NuVents-ios/DetailView.swift
+++ NuVents-ios/DetailView.swift
@@ -11,8 +11,6 @@ import Foundation
 class DetailView: UIViewController, UIWebViewDelegate {
     
     internal var json: JSON = JSON("") // Event variable to be passed
-    var webView: UIWebView!
-    var panoView: GMSPanoramaView!
     let bounds = UIScreen.mainScreen().bounds
     
     override func viewDidLoad() {
@@ -24,31 +22,15 @@ class DetailView: UIViewController, UIWebViewDelegate {
         let file = dir.stringByReplacingOccurrencesOfString("tmp/tmp", withString: "") + "data"
         "\(json)".writeToFile(file, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
         
-        loadPartialView()
-    }
-    
-    // Load detail view
-    func loadDetailView() {
-        var baseURL = NuVentsBackend.getResourcePath("tmp", type: "tmp") // Base URL: resources dir
+        // Load webview
+        var webView = UIWebView()
+        webView.delegate = self
+        webView.frame = CGRectMake(0, 0, bounds.width, bounds.height)
+        self.view.addSubview(webView)
+        var baseURL = NuVentsBackend.getResourcePath("tmp", type: "tmp")
         baseURL = baseURL.stringByReplacingOccurrencesOfString("tmp/tmp", withString: "")
         let fileURL = NuVentsBackend.getResourcePath("detailView", type: "html")
         let htmlStr = NSString(contentsOfFile: fileURL, encoding: NSUTF8StringEncoding, error: nil) as! String
-        webView.loadHTMLString(htmlStr, baseURL: NSURL(fileURLWithPath: fileURL))
-    }
-    
-    // Load partial view
-    func loadPartialView() {
-        // Add webview
-        if (webView == nil) {
-            webView = UIWebView()
-            webView.delegate = self
-            webView.frame = CGRectMake(0, 0, bounds.width, bounds.height)
-            self.view.addSubview(webView)
-        }
-        var baseURL = NuVentsBackend.getResourcePath("tmp", type: "tmp") // Base URL: resources dir
-        baseURL = baseURL.stringByReplacingOccurrencesOfString("tmp/tmp", withString: "")
-        let fileURL = NuVentsBackend.getResourcePath("partialView", type: "html") // Partial view html
-        var htmlStr = NSString(contentsOfFile: fileURL, encoding: NSUTF8StringEncoding, error: nil) as! String
         webView.loadHTMLString(htmlStr, baseURL: NSURL(fileURLWithPath: baseURL))
     }
     
@@ -56,13 +38,7 @@ class DetailView: UIViewController, UIWebViewDelegate {
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         
         let reqStr = request.URL?.absoluteString
-        if reqStr!.rangeOfString("openDetailView://") != nil {
-            loadDetailView()
-            return false
-        } else if reqStr!.rangeOfString("closeDetailView://") != nil {
-            loadPartialView()
-            return false
-        } else if reqStr!.rangeOfString("closePartialView://") != nil {
+        if reqStr!.rangeOfString("closeDetailView://") != nil {
             self.dismissViewControllerAnimated(true, completion: nil)
             return false
         } else {
