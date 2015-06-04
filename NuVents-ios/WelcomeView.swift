d6cde9ac2e94d233da27afac70ffed585dc9899a
Hersh Amin <h.amin@utexas.edu>
2015-06-04 14:29:14 -0500

    Changed initial search radius to 5000 meters

--- NuVents-ios/WelcomeView.swift
+++ NuVents-ios/WelcomeView.swift
@@ -42,7 +42,7 @@ class WelcomeViewController: UIViewController, NuVentsBackendDelegate, UIWebView
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         if (serverConn) { // Only use when connected to server
             var latestLoc:CLLocation = locations[locations.count - 1] as! CLLocation
-            api?.getNearbyEvents(latestLoc.coordinate, radius: 100)
+            api?.getNearbyEvents(latestLoc.coordinate, radius: 5000) // Search within 5000 meters
             locationManager.stopUpdatingLocation()
         }
     }
