4f08d6d6b94f5f1d59770dff060765d183750820
Hersh Amin <h.amin@utexas.edu>
2015-07-08 20:35:47 -0500

    Changed handling for adding events in detail view, Minor bug fix in detail view: text field representing x miles away! was editable

--- NuVents-ios/DetailView.swift
+++ NuVents-ios/DetailView.swift
@@ -106,61 +106,76 @@ class DetailViewController: UIViewController, UIWebViewDelegate, EKEventEditView
     func openCalendarApp() {
         // Event store (calendar) config
         var eventStore:EKEventStore = EKEventStore()
+        var calendarAuthStatus = EKAuthorizationStatus.Denied
         
-        // Add to calendar
-        eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion: {
-            (granted, error) in
-            if (granted) && (error == nil) {
-                var event:EKEvent = EKEvent(eventStore: eventStore)
-                
-                // Set event attributes
-                event.calendar = eventStore.defaultCalendarForNewEvents
-                event.title = self.event["title"].stringValue
-                event.startDate = NSDate(timeIntervalSince1970: self.event["time"]["start"].doubleValue)
-                event.endDate = NSDate(timeIntervalSince1970: self.event["time"]["end"].doubleValue)
-                event.notes = self.event["description"].stringValue
-                event.location = self.event["address"].stringValue
-                event.URL = NSURL(string: self.event["website"].stringValue)
-                
-                // Open eventkit UI so user can save to calendar
-                var eventController:EKEventEditViewController = EKEventEditViewController()
-                eventController.eventStore = eventStore
-                eventController.event = event
-                eventController.editViewDelegate = self
-                
-                self.presentViewController(eventController, animated: true, completion: nil)
-            } else {
-                if (!granted) { // Permission not granted
-                    if objc_getClass("UIAlertController") != nil { // ios 8+
-                        let message:String = "In order to show events, Please open this app's settings and turn on Calendars access"
-                        var alert = UIAlertController(title: "Calendar Access Denied", message: message, preferredStyle: UIAlertControllerStyle.Alert)
-                        let cancelAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil)
-                        alert.addAction(cancelAction)
-                        let openAction = UIAlertAction(title: "Open Settings", style: UIAlertActionStyle.Default) { (action) in
-                            let url = NSURL(string: UIApplicationOpenSettingsURLString)
-                            UIApplication.sharedApplication().openURL(url!)
-                        }
-                        alert.addAction(openAction)
-                        self.presentViewController(alert, animated: true, completion: nil)
-                    } else { // ios 7
-                        let message:String = "In order to show events, Please open settings, go to Privacy > Calendars > NuVents and Turn on Calendars"
-                        var alert = UIAlertView(title: "Calendar Access Denied", message: message, delegate: nil, cancelButtonTitle: "OK")
-                        alert.show()
-                    }
-                } else { // Error
-                    if objc_getClass("UIAlertController") != nil { // ios 8+
-                        var alert = UIAlertController(title: "Error Accessing Calendars", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
-                        let cancelAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil)
-                        alert.addAction(cancelAction)
-                        self.presentViewController(alert, animated: true, completion: nil)
-                    } else { // ios 7
-                        var alert = UIAlertView(title: "Error Accessing Calendars", message: error.localizedDescription, delegate: nil, cancelButtonTitle: "OK")
-                        alert.show()
-                    }
+        // Get calendar authorization status & act accordingly
+        var showAlert = false
+        var title:String = ""
+        var message:String = ""
+        calendarAuthStatus = EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent)
+        if (calendarAuthStatus == EKAuthorizationStatus.NotDetermined) { // Ask for access then add
+            eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion: {
+                (granted, error) in
+                if (granted) {
+                    dispatch_async(dispatch_get_main_queue(), {self.addEventToCalendar(eventStore)})
                 }
+            })
+        } else if (calendarAuthStatus == EKAuthorizationStatus.Authorized) { // Add event
+            self.addEventToCalendar(eventStore)
+        } else if (calendarAuthStatus == EKAuthorizationStatus.Denied) { // Show instructions to grant access
+            showAlert = true
+            title = "Calendar Access Disabled"
+            if objc_getClass("UIAlertController") != nil { // ios 8+
+                message = "In order to add events, Please open this app's settings and turn on Calendar access"
+            } else { // ios 7
+                message = "In order to add events, Please open settings, go to Privacy > Calendars > NuVents and Turn on Calendars"
             }
-        })
+        } else if (calendarAuthStatus == EKAuthorizationStatus.Restricted) { // Notify regarding parental controls
+            showAlert = true
+            title = "Calendar Access Restricted"
+            message = "Parental Controls might be enabled or Calendars might be disabled on your device, if possible, Please open this app's settings to disable Parental Controls or enable Calendars"
+        }
         
+        // Show Alert view
+        if (showAlert) {
+            if objc_getClass("UIAlertController") != nil { // ios 8+
+                var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+                let cancelAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil)
+                alert.addAction(cancelAction)
+                let openAction = UIAlertAction(title: "Open Settings", style: UIAlertActionStyle.Default) { (action) in
+                    let url = NSURL(string: UIApplicationOpenSettingsURLString)
+                    UIApplication.sharedApplication().openURL(url!)
+                }
+                alert.addAction(openAction)
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else { // ios 7
+                var alert = UIAlertView(title: title, message: message, delegate: nil, cancelButtonTitle: "OK")
+                alert.show()
+            }
+        }
+        
+    }
+    
+    // Helper function to add event to calendar
+    func addEventToCalendar(eventStore: EKEventStore) {
+        var event:EKEvent = EKEvent(eventStore: eventStore)
+        
+        // Set event attributes
+        event.calendar = eventStore.defaultCalendarForNewEvents
+        event.title = self.event["title"].stringValue
+        event.startDate = NSDate(timeIntervalSince1970: self.event["time"]["start"].doubleValue)
+        event.endDate = NSDate(timeIntervalSince1970: self.event["time"]["end"].doubleValue)
+        event.notes = self.event["description"].stringValue
+        event.location = self.event["address"].stringValue
+        event.URL = NSURL(string: self.event["website"].stringValue)
+        
+        // Open eventkit UI so user can save to calendar
+        var eventController:EKEventEditViewController = EKEventEditViewController()
+        eventController.eventStore = eventStore
+        eventController.event = event
+        eventController.editViewDelegate = self
+        
+        self.presentViewController(eventController, animated: true, completion: nil)
     }
     
     // Event kit (Calendar) edit view delegate
