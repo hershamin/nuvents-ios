c8a6772df4595e5d058ae260a965be89043ca215
Hersh Amin <h.amin@utexas.edu>
2015-07-09 19:32:34 -0500

    Added library to perform update check on new app versions available and prompt user accordingly

--- NuVents-ios/AppDelegate.swift
+++ NuVents-ios/AppDelegate.swift
@@ -19,8 +19,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         let googleMapsApiKey = "AIzaSyCoxB_xocbfinGPLJdiexNsXfW_o7PqeEg"
+        let appStoreID = "792991234"
         Fabric.with([Crashlytics()])
         GMSServices.provideAPIKey(googleMapsApiKey)
+        // Siren Framework for notifying users when new updates are available
+        let siren = Siren.sharedInstance
+        siren.appID = appStoreID
+        siren.majorUpdateAlertType = SirenAlertType.Option  // x.1.1
+        siren.minorUpdateAlertType = SirenAlertType.Skip    // 1.x.1
+        siren.patchUpdateAlertType = SirenAlertType.Force   // 1.1.x
+        siren.checkVersion(SirenVersionCheckType.Immediately) // Check for new app version at app start
+        
         return true
     }
 
@@ -36,10 +45,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        Siren.sharedInstance.checkVersion(SirenVersionCheckType.Immediately) // Check for new app version
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        Siren.sharedInstance.checkVersion(SirenVersionCheckType.Daily) // Perform daily checks for new version
     }
 
     func applicationWillTerminate(application: UIApplication) {
