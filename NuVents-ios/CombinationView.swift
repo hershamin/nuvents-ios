50f05edb9d08348b7e30953fe074080a286ce8c6
Hersh Amin <h.amin@utexas.edu>
2015-08-01 15:12:20 -0500

    Added required view controllers

---
+++ NuVents-ios/CombinationView.swift
@@ -0,0 +1,14 @@
+
+//
+//  CombinationView.swift
+//  NuVents-ios
+//
+//  Created by hersh amin on 8/1/15.
+//  Copyright (c) 2015 NuVents. All rights reserved.
+//
+
+import Foundation
+
+class CombinationViewController: UIViewController {
+    //
+}
---
+++ NuVents-ios/RequestView.swift
@@ -0,0 +1,13 @@
+//
+//  RequestView.swift
+//  NuVents-ios
+//
+//  Created by hersh amin on 8/1/15.
+//  Copyright (c) 2015 NuVents. All rights reserved.
+//
+
+import Foundation
+
+class RequestViewController: UIViewController {
+    //
+}
