48e9743e37efbeb095ff5687635ef61c261c986d
Aaron Dack <Aaron@Aarons-MacBook-Pro.local>
2015-08-06 18:51:15 -0500

    Adding some initial code for cat

--- NuVents-ios/CategoryView.swift
+++ NuVents-ios/CategoryView.swift
@@ -10,10 +10,34 @@ import Foundation
 
 class CategoryViewController: UIViewController {
     
+    @IBOutlet var imageView: UIImageView!
+    
+    let reuseIdentifier = "Cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.redColor()
+        NuVentsHelper.getResourcePath("icon", type: String(), override: false)
+        
+    }
+    //Setup the collection view.
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // 1
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // 5
+        return 5
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! UICollectionViewCell
+        
+        //Configure the cell's with the category icon images.
+        
+        return cell
     }
     
     // Restrict to portrait only
