c9fab30b33bbd15cb23673858beab51ca0e8f96a
hersh <h.amin@utexas.edu>
2015-08-12 21:15:50 -0500

    Added ability to highlight multiple category icons

--- NuVents-ios/CategoryView.swift
+++ NuVents-ios/CategoryView.swift
@@ -40,25 +40,58 @@ class CategoryViewController: UIViewController, UICollectionViewDataSource, UICo
         return iconList.count
     }
     
+    // Called to set cell appearence
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell: CategoryViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CategoryViewCell
+        // Check for intention to set background
+        let background:Bool = iconList[indexPath.row]["bg"].boolValue
         
-        //Set the image in the cell
-        let filePathImage = NuVentsHelper.getResourcePath(iconList[indexPath.row]["value"].stringValue, type: "categoryIcon")
-        cell.imageCell.image = UIImage(contentsOfFile: filePathImage)
+        // Get appropriate collection view cell
+        let cell: CategoryViewCell = self.myCollectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CategoryViewCell
         
-        //Give the cell a label
+        // Basic cell appearence
+        var bgColor: UIColor! // Cell background color
+        var borderColor: CGColor! // Cell border color
+        var textColor: UIColor! // Cell text color
+        cell.layer.cornerRadius = 3
+        cell.layer.borderWidth = 2.0
+        var imgType: String! // Cell image type (highlighted or normal)
+        
+        // 2 Different cell appearences
+        if (background) { // Pink background to match branding
+            bgColor = UIColor(red: 0.91, green: 0.337, blue: 0.427, alpha: 1) // #E8566D
+            borderColor = bgColor.CGColor
+            imgType = "categoryIconHighlighted"
+            textColor = UIColor.whiteColor()
+        } else {
+            bgColor = UIColor.clearColor()
+            borderColor = UIColor(red: 0.84, green: 0.844, blue: 0.852, alpha: 1).CGColor // Light gray
+            imgType = "categoryIcon"
+            textColor = UIColor.blackColor()
+        }
+        
+        // Set appearence
+        cell.backgroundColor = bgColor
+        cell.layer.borderColor = borderColor
+        let imgPath = NuVentsHelper.getResourcePath(iconList[indexPath.row]["value"].stringValue, type: imgType)
+        cell.imageCell.image = UIImage(contentsOfFile: imgPath)
+        cell.labelCell.textColor = textColor
         cell.labelCell.text = iconList[indexPath.row]["name"].stringValue
         
-        //Produce a border for the cells and color them.
-        var color: UIColor = UIColor(red: 0.84, green: 0.844, blue: 0.852, alpha: 1)
-        
-        cell.backgroundColor = UIColor.clearColor()
-        cell.layer.borderColor = color.CGColor
-        cell.layer.borderWidth = 2.0
-        cell.layer.cornerRadius = 3
-        
         return cell
+    }
+    
+    // Cell tapped
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let bgIntent:Bool = iconList[indexPath.row]["bg"].boolValue
+        if (bgIntent) {
+            iconList[indexPath.row]["bg"] = false
+            collectionView.reloadItemsAtIndexPaths([indexPath])
+            collectionView.deselectItemAtIndexPath(indexPath, animated: true)
+        } else {
+            iconList[indexPath.row]["bg"] = true
+            collectionView.reloadItemsAtIndexPaths([indexPath])
+            collectionView.deselectItemAtIndexPath(indexPath, animated: true)
+        }
     }
     
     //Make function for notification
