0ce818a74737263ca89da8058ec9a27e51f0fed5
hersh <h.amin@utexas.edu>
2015-08-21 23:49:10 -0500

    Added custom transition of swipe from left when opening detail view

---
+++ NuVents-ios/DetailViewUnwindSegue.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewUnwindSegue.swift
+//  NuVents-ios
+//
+//  Created by Hersh on 8/21/15.
+//  Copyright (c) 2015 NuVents. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewUnwindSegue: UIStoryboardSegue {
+
+}
