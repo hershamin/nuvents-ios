40b1d1f620d9d178d8d67a63c7807638080c8825
Aaron Dack <adackbusiness@gmail.com>
2015-09-12 16:37:55 -0500

    Adding background color and image to RequestView

--- NuVents-ios/RequestView.swift
+++ NuVents-ios/RequestView.swift
@@ -11,6 +11,7 @@ import Foundation
 class RequestViewController: UIViewController {
     
     @IBOutlet var backBtn:UIButton!
+    @IBOutlet weak var illustrationImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +19,19 @@ class RequestViewController: UIViewController {
         
         // Back button functionality
         backBtn.addTarget(self, action: "backBtnPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        //Convert the background color
+        self.view.backgroundColor = UIColor(red:0.61, green:0.34, blue:0.65, alpha:1.0) //#9c57a6
+        
+        //Load the image
+        illustrationImage.image = UIImage(named: "RequestIllustration")
     }
     
     // Back button action
     func backBtnPressed(sender:UIButton!) {
         self.performSegueWithIdentifier("unwindRequestView", sender: nil)
     }
+    
     
     // Restrict to portrait only
     override func supportedInterfaceOrientations() -> Int {
