7b9e9ad240c92edb9e39d2abce81aa85fc906075
Aaron Dack <adackbusiness@gmail.com>
2015-09-12 22:33:58 -0500

    Added UI for email address and button bring it here

--- NuVents-ios/RequestView.swift
+++ NuVents-ios/RequestView.swift
@@ -15,6 +15,9 @@ class RequestViewController: UIViewController {
     @IBOutlet weak var nuventsMessage: UILabel!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var textForName: UITextField!
+    @IBOutlet weak var textForEmail: UITextField!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var bringItHere: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,13 +37,21 @@ class RequestViewController: UIViewController {
         nuventsMessage.numberOfLines = 0;
         nuventsMessage.textAlignment = NSTextAlignment.Center
         
-        //Add name
+        //Add name and email
         self.name.text = "NAME"
-        //Change attributes of textfield, and put placeholder text
+        self.email.text = "EMAIL"
+        //Change attributes of name and email textfield, and put placeholder text
         textForName.alpha = 0.8
-       // textForName.attributedPlaceholder = "First Name"
+        textForEmail.alpha = 0.8
+        //Edit the bringItHere button
+        bringItHere.layer.borderColor = UIColor.whiteColor().CGColor
+        bringItHere.layer.borderWidth = 1.75
+        bringItHere.layer.cornerRadius = bringItHere.bounds.size.height/4
+        
+        //Init some actions on press
         
     }
+    
     
     // Back button action
     func backBtnPressed(sender:UIButton!) {
